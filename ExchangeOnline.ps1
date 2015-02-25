a519e988742f47af2a5bf319c49556dc2074960e
kseverudscc <kseverud@shoreline.edu>
2015-02-25 08:07:47 -0800

    ExchangeOnline

---
+++ ExchangeOnline.ps1
@@ -0,0 +1,2 @@
+﻿$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
